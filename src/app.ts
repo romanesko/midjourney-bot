f55b9dc358d3c1d1c47c6f4ca095d0852352aae3
Roman Bykovskiy <8026787@gmail.com>
2023-06-08 20:58:12 +0300

    refactoring

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,14 @@ import {storage} from "./storage";
 const bot: Telegraf<Context<Update>> = new Telegraf(process.env.BOT_TOKEN as string);
 
 bot.start(async (ctx) => {
-    ctx.reply('Welcome ' + ctx.from.first_name)
-    if (!(await storage.knownPersonsHas(ctx.from.id))) {
-        ctx.reply("You are NOT in the list of known persons, you can't use this bot, sorry")
-        return
+    try {
+        await processMessage(ctx, 'Welcome ' + ctx.from.first_name)
+        if (!(await storage.knownPersonsHas(ctx.from.id))) {
+            await processMessage(ctx, "You are NOT in the list of known persons, you can't use this bot, sorry")
+            return
+        }
+    } catch (e) {
+        console.log(e)
     }
 });
 
@@ -28,12 +32,22 @@ const getApi = () => {
     return new Api()
 }
 
+const processMessage = async (ctx: Context<Update>, msg: string) => {
+    console.log('sending message:', msg)
+    try {
+        await ctx.replyWithHTML(msg)
+    } catch (e) {
+        console.log("Can't send message", e)
+    }
+}
+
 const sendImageWithButtons = async (ctx: Context<Update>, msg: MJMessage, caption?: string) => {
     storage.cacheMessage(msg)
 
     const file = await downloadImage(msg.uri)
+    console.log('sending photo with caption:', caption)
     await ctx.replyWithPhoto(Input.fromLocalFile(file), {
-        caption,
+        // caption,
         ...Markup.inlineKeyboard([
             [
                 Markup.button.callback("U1", `${msg.id}:U1`),
@@ -60,41 +74,44 @@ bot.command('auth', async (ctx) => {
         return
     }
     await storage.addKnownPerson(ctx.from.id)
-    ctx.reply('You can use /imagine now')
+    await processMessage(ctx, 'You can use /imagine now')
 })
 
 bot.command('ping', async (ctx) => {
-    ctx.reply('pong')
+    await processMessage(ctx, 'pong')
 })
 
 bot.command('imagine', async (ctx) => {
 
     if (!(await storage.knownPersonsHas(ctx.from.id))) {
-        ctx.reply('You are NOT in the list of known persons, you can use this bot, sorry')
+        await processMessage(ctx, "You are NOT in the list of known persons, you can't use this bot, sorry")
         return
     }
 
-    const prompt = ctx.update.message?.text.slice(9)
+    let prompt = ctx.update.message?.text.slice(9)
     if (!prompt) {
-        ctx.reply('You should provide prompt, like\n`/imagine cat flying in the clouds in the style of Edward Tufte`', {parse_mode: 'MarkdownV2'})
+        await processMessage(ctx, 'You should provide prompt, like\ncode>/imagine cat flying in the clouds in the style of Edward Tufte</code>')
         return
     }
+
+    prompt = prompt.replace('—', '--')
+
     console.log('PROMPT:', prompt)
-    ctx.reply(`I'm starting to come up with: *${prompt}*\nWait a bit, it may take some time`, {parse_mode: 'MarkdownV2'})
+    await processMessage(ctx, `I'm starting to come up with: <b>${prompt}</b>\nWait a bit, it may take some time`)
     try {
         const apiResponse = await getApi().imagine(prompt)
         if (!apiResponse) {
             console.log('Api response in null');
-            ctx.reply(`Something went wrong (`)
+            await processMessage(ctx, `Something went wrong (`)
             return;
         }
         if (apiResponse.progress !== 'done') {
-            ctx.reply(`Something went wrong (`)
+            await processMessage(ctx, `Something went wrong (`)
             return
         }
         await sendImageWithButtons(ctx, apiResponse, prompt)
     } catch (e) {
-        ctx.reply(`Something went wrong (`)
+        await processMessage(ctx, `Something went wrong (`)
         console.log(e)
         return
     }
@@ -108,44 +125,44 @@ bot.on('callback_query', async (ctx) => {
 
         const msg = await storage.getCachedMessage(id)
         if (!msg) {
-            ctx.reply('There was a problem with cache, try to request image again')
+            await processMessage(ctx, 'There was a problem with cache, try to request image again')
             return
         }
 
         const [t, num] = cmd.split('')
 
         if (t === 'U') {
-            ctx.reply('Upscale version ' + num + '\nWait a bit, it may take some time')
+            await processMessage(ctx, 'Upscale version ' + num + '\nWait a bit, it may take some time')
             const upscaleMsg = await getApi().upscale(msg, num)
             if (!upscaleMsg) {
                 console.log('UPSCALE: Api response in null')
-                ctx.reply('Something went wrong')
+                await processMessage(ctx, 'Something went wrong')
                 return
             }
             const file = await downloadImage(upscaleMsg.uri)
             await ctx.replyWithDocument(Input.fromLocalFile(file))
             fs.unlinkSync(file)
         } else if (t === 'V') {
-            ctx.reply('New variations for version ' + num + '\nWait a bit, it may take some time')
+            await processMessage(ctx, 'New variations for version ' + num + '\nWait a bit, it may take some time')
             const variantMsg = await getApi().variant(msg, num)
             if (!variantMsg) {
                 console.log('VARIATIONS: Api response in null')
-                ctx.reply('Something went wrong')
+                await processMessage(ctx, 'Something went wrong')
                 return
             }
             await sendImageWithButtons(ctx, variantMsg)
         } else {
-            ctx.reply('Unknown command')
+            await processMessage(ctx, 'Unknown command')
         }
     } catch (e) {
         console.log(e)
-        ctx.reply('Something went wrong')
+        await processMessage(ctx, 'Something went wrong')
     }
 
 });
 bot.launch();
 
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+// process.once('SIGINT', () => bot.stop('SIGINT'));
+// process.once('SIGTERM', () => bot.stop('SIGTERM'))
 console.log(new Date(), 'Started')
